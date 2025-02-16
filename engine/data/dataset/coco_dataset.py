db711ccfdd2923d6fe7cec7b511d58011bb54dd9
johnny-lang <85885766+johnny-lang@users.noreply.github.com>
2025-02-16 23:09:59 +0700

    change

--- engine/data/dataset/coco_dataset.py
+++ engine/data/dataset/coco_dataset.py
@@ -130,7 +130,9 @@ class ConvertCocoPolysToMask(object):
 
         category2label = kwargs.get('category2label', None)
         if category2label is not None:
-            labels = [category2label[obj["category_id"]] for obj in anno]
+            # labels = [category2label[obj["category_id"]] for obj in anno]
+            labels = [category2label[obj["category_id"]] for obj in anno if obj["category_id"] in category2label]
+
         else:
             labels = [obj["category_id"] for obj in anno]
 
